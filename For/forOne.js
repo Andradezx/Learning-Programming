34fd9228a54a2278fa83cb95f8047f720cb06f45
Andradezx <vitorminecrfit@gmail.com>
2024-08-15 17:17:51 -0300

    nn

--- For/forOne.js
+++ For/forOne.js
@@ -1,6 +1,6 @@
-for (var contador = 0; contador <=5; contador++) {
+var soma = 0 
+for (var contador = 1; contador <=5; contador++) {
    var number = parseInt(prompt("Digite um numero"))
-   var soma = number+number
-   document.write(`${soma}`)
-
+   var soma = number+soma
 }
+document.write(`${soma}`)
