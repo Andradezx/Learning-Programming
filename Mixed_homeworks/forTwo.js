602695098ca5a1316f0476770c598b936d2c1cf0
Andradezx <vitorminecrfit@gmail.com>
2024-08-16 16:28:38 -0300

    new

--- Mixed_homeworks/forTwo.js
+++ Mixed_homeworks/forTwo.js
@@ -1,6 +1,6 @@
 var number = parseInt(prompt("Digite um numero "))
 
 for (var contador = 1; contador <=10; contador++){
-    document.write(` ${number}x${contador}= ${contador*number} <br>`)
+    document.write(` ${number}x${contador}= ${number*contador} <br>`)
 
 }
