87d0ec6654f79cb57aafb03948ecfcb00701df4d
Andradezx <vitorminecrfit@gmail.com>
2024-08-21 15:00:26 -0300

    new

---
+++ while/whileThree.js
@@ -0,0 +1,23 @@
+var cont = 0
+
+var max = parseFloat(0)
+var min = parseFloat(Infinity)
+var soma = parseFloat(0)
+
+while(cont < 10){
+   var sac = parseFloat(prompt("Digite o quanto deseja sacar "))
+   var soma = (sac+soma)
+   cont+=1
+
+ if (sac > max){
+    var max = sac 
+ }
+  if (sac < min) {
+    var min = sac 
+
+ }   
+ 
+
+ }
+
+document.write(`Maior valor sacado ${max} <br> Meno valor sacado ${min} <br> O total sacado no dia é ${soma}<br> A media sacada no dia foi ${soma/cont}`)
---
+++ while/whileTwo.js
@@ -0,0 +1,8 @@
+var cont = 0
+//var graus = parent(prompt("Digite os graus"))
+
+while(cont <= 100){
+  document.write(`${cont}°C  ${(cont*1.8)+31.} <br>`)
+  cont+=10
+
+}
