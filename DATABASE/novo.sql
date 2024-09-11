9eff2c34853958e5393da709e48ef4c03165ef03
Andradezx <vitorminecrfit@gmail.com>
2024-09-11 16:48:12 -0300

    new

--- DATABASE/novo.sql
+++ DATABASE/novo.sql
@@ -18,9 +18,30 @@ salario_ator DOUBLE NOT NULL,
 sexo_ator VARCHAR(1) NOT NULL
 );
 
+CREATE TABLE tbPersonagem (
+codigo_personagem SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY  NOT NULL,
+nome_personagem varchar(50) NOT NULL,
+idade_personagem TINYINT UNSIGNED NOT NULL,
+situacao_financeira_personagem VARCHAR(100),
+codigo_autor SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+CONSTRAINT PK_tbPersonaAutor FOREIGN KEY (codigo_autor) REFERENCES tbAutor(codigo_autor)
+);
+
+CREATE TABLE tbCapitulo(
+codigo_capitulo SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+nome_capitulo VARCHAR(30) NOT NULL,
+data_exibicao_capitulo DATE NOT NULL,
+codigo_novela SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+CONSTRAINT pk_tbCapituloNovela FOREIGN KEY (codigo_novela) REFERENCES tbNovela(codigo_novela)
+
+);
+
+
 CREATE TABLE tbNovelaPersonagem(
 codigo_novela SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
 codigo_personagem SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
 PRIMARY KEY (codigo_novela,codigo_personagem),
-CONSTRAINT pk_tbNovelaPersonagem FOREIGN  KEY ()
+CONSTRAINT pk_tbNovelaPersonagem_Personagem FOREIGN  KEY (codigo_personagem)  REFERENCES tbPersonagem(codigo_personagem),
+CONSTRAINT pk_tbNovelaPersonagem_Novela  FOREIGN KEY (codigo_novela)  REFERENCES tbNovela(codigo_novela)
 );
+
