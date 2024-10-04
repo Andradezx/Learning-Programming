6ab7ae346fa8615df355b967c92d20507ff53ec7
Andradezx <vitorminecrfit@gmail.com>
2024-10-04 16:15:10 -0300

    new

--- PYTHON/Aula01/app01/views.py
+++ PYTHON/Aula01/app01/views.py
@@ -5,10 +5,22 @@ from .models import User
 
 
 def login_user(request):
-    return
+    if request.method == 'POST':
+        username = request.POST['email']
+        userpassword = request.POST['password']
+        try: 
+            success = User.objects.get(User = username, password = userpassword)
+            request.session['userID'] = success.id
+            return redirect('home')
+        except User.DoesNotExist:
+            messages.error(request,"Usuario Invalido")
+    return   render(request,'index')
 
 def logout_user(request):
     return
 
 def check_login(request):
-    return
+    if 'UserID' in request.session:
+        return render(request,'home')
+    else:
+        return redirect('index')
