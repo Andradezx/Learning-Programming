8b29202ea91eb5cbd6d176b62bbfed382f3519c3
Andradezx <vitorminecrfit@gmail.com>
2024-10-11 16:02:52 -0300

    new

--- PROJETOS-PYTHON/register/cadastre/views.py
+++ PROJETOS-PYTHON/register/cadastre/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import User
-from django.contrib.auth import logout
+from .forms import UserForm
 
 
 def register_user(request):
     if request.method == 'POST':
-        form = Userforms(request.POST)
+        form = UserForm(request.POST)
 
         if form.is_valid():
             form.save()
